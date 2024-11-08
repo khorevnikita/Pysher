b7fd1ce65ad3a4dbaa8e697f8d70c11fba527ef5
Nikita Khorev <codenrock.nk@gmail.com>
2024-11-08 23:23:29 +0300

    совместимая версия

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-VERSION = "1.0.11"
+VERSION = "1.0.12"
 
 
 def readme():
--- src/pysher_khonik/pusher.py
+++ src/pysher_khonik/pusher.py
@@ -1,12 +1,13 @@
-from channel import Channel
-from connection import Connection
+from .channel import Channel
+from .connection import Connection
 import requests
 import hashlib
 import hmac
 import logging
 import json
 
-VERSION = '1.0.11'
+VERSION = '1.0.12'
+
 
 class Pusher(object):
     host = "ws.pusherapp.com"
@@ -14,7 +15,9 @@ class Pusher(object):
     protocol = 6
 
     def __init__(self, key, cluster="", secure=True, secret="", auth_endpoint=None, auth_endpoint_headers={},
-                 user_data=None, log_level=logging.INFO, daemon=True, port=443, reconnect_interval=10, custom_host="", auto_sub=False, http_proxy_host="", http_proxy_port=0, http_no_proxy=None, http_proxy_auth=None, **thread_kwargs):
+                 user_data=None, log_level=logging.INFO, daemon=True, port=443, reconnect_interval=10, custom_host="",
+                 auto_sub=False, http_proxy_host="", http_proxy_port=0, http_no_proxy=None, http_proxy_auth=None,
+                 **thread_kwargs):
         if cluster:
             self.host = "ws-{cluster}.pusher.com".format(cluster=cluster)
         else:
@@ -28,7 +31,12 @@ class Pusher(object):
         self.channels = {}
         self.url = self._build_url(secure, port, custom_host)
         reconnect_handler = self._reconnect_handler if auto_sub else None
-        self.connection = Connection(self._connection_handler, self.url, reconnect_handler=reconnect_handler, log_level=log_level, daemon=daemon, reconnect_interval=reconnect_interval, socket_kwargs=dict(http_proxy_host=http_proxy_host, http_proxy_port=http_proxy_port, http_no_proxy=http_no_proxy, http_proxy_auth=http_proxy_auth, ping_timeout=100), **thread_kwargs)
+        self.connection = Connection(self._connection_handler, self.url, reconnect_handler=reconnect_handler,
+                                     log_level=log_level, daemon=daemon, reconnect_interval=reconnect_interval,
+                                     socket_kwargs=dict(http_proxy_host=http_proxy_host,
+                                                        http_proxy_port=http_proxy_port, http_no_proxy=http_no_proxy,
+                                                        http_proxy_auth=http_proxy_auth, ping_timeout=100),
+                                     **thread_kwargs)
 
     @property
     def key_as_bytes(self):
@@ -100,7 +108,8 @@ class Pusher(object):
             h = hmac.new(self.secret_as_bytes, subject.encode('utf-8'), hashlib.sha256)
             auth_key = "{}:{}".format(self.key, h.hexdigest())
         elif self.auth_endpoint:
-            request_data = {"channel_name": channel_name, "socket_id": self.connection.socket_id, "user_data": self.user_data}
+            request_data = {"channel_name": channel_name, "socket_id": self.connection.socket_id,
+                            "user_data": self.user_data}
             response = requests.post(self.auth_endpoint, data=request_data, headers=self.auth_endpoint_headers)
             assert response.status_code == 200, "Failed to get auth token from {}".format(self.auth_endpoint)
             auth_key = response.json()["auth"]
